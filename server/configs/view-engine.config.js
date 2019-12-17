933d0942c033f756e12399eb18deee369f12b362
alejandro alarco <alejandroalarco@alejandros-MacBook-Pro-3040.local>
2019-12-17 11:39:05 +0100

    Deploying

--- server/configs/view-engine.config.js
+++ server/configs/view-engine.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const favicon = require('serve-favicon');
 const express = require('express');
 
 module.exports = app => {
@@ -14,6 +13,5 @@ module.exports = app => {
     app.set('views', path.join(__dirname, '..', 'views'));
     app.set('view engine', 'hbs');
     app.use(express.static(path.join(__dirname, '..', 'public')));
-    app.use(favicon(path.join(__dirname, '..', 'public', 'images', 'favicon.ico')));
 
 }
